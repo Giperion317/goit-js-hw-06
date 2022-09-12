11013fc72363468e7da01a68adc8291b760e7fea
Anatolii Pismennyi <105397905+Giperion317@users.noreply.github.com>
2022-09-13 01:56:19 +0200

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -2,50 +2,53 @@
 // количество элементов в input и нажимает кнопку Создать, после чего рендерится
 // коллекция.При нажатии на кнопку Очистить, коллекция элементов очищается.
 
-// Создай функцию createBoxes(amount), которая принимает один параметр - число. 
-// Функция создает столько < div >, сколько указано в amount и добавляет их в 
+// Создай функцию createBoxes(amount), которая принимает один параметр - число.
+// Функция создает столько < div >, сколько указано в amount и добавляет их в
 // div#boxes.
 
 // Размеры самого первого <div> - 30px на 30px.
 // Каждый элемент после первого, должен быть шире и выше предыдущего на 10px.
-// Все элементы должены иметь случайный цвет фона в формате HEX.Используй готовую 
+// Все элементы должены иметь случайный цвет фона в формате HEX.Используй готовую
 // функцию getRandomHexColor для получения цвета.
 
-// Создай функцию destroyBoxes(), которая очищает содержимое div#boxes, тем самым 
+// Создай функцию destroyBoxes(), которая очищает содержимое div#boxes, тем самым
 // удаляя все созданные элементы.
 
 const optionsInput = document.querySelector('input');
 
 const createBtn = document.querySelector('button[data-create]');
 
-const destroyBtn = document.querySelector('button[data-destroy]')
+const destroyBtn = document.querySelector('button[data-destroy]');
 
-const boxes = document.querySelector("#boxes")
+const boxes = document.querySelector('#boxes');
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+optionsInput.addEventListener('blur', createBoxes);
+
+destroyBtn.addEventListener('click', destroyBoxes);
+
+function createBoxes(amount) {
+  const amountBoxes = amount.target.value;
+  createBtn.addEventListener('click', () => {
+    for (let i = 0; i < amountBoxes; i += 1) {
+      const div = document.createElement('div');
+      div.classList.add('new-box');
+      div.style.width = '30px';
+      div.style.height = '30px';
+      div.style.backgroundColor = getRandomHexColor();
+
+      boxes.appendChild(div);
+    };
+  });
 };
 
-optionsInput.addEventListener('input', createBoxes)
-
-
-function createBoxes(amount) { 
-  createBtn.addEventListener('click', () => {
-    let amountBoxes = amount.target.value;
-for (let i = 1; i <= amountBoxes; i+=1) {
-  let div = document.createElement('div');
-  div.width = 30px;
-  div.height = 30px;
-  div.backgroundColor = 'tomato';
-
-    
+function destroyBoxes() {
+  const newBox = document.querySelectorAll('.new-box');
+  
+  newBox.forEach(el => {
+    el.remove();
+  });
 }
-
-boxes.append(div);
-
-  })
-}
-
-
-
-
